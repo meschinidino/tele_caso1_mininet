932ea7fee881c5e4c41816cdb1875aaa43470f27
Dino Meschini <francodmsoloing@gmail.com>
2025-05-15 14:49:37 -0300

    commit fixes

--- caso_1.py
+++ caso_1.py
@@ -4,45 +4,79 @@ from mininet.cli import CLI
 from mininet.link import TCLink
 from mininet.log import setLogLevel, info
 
-def networkTopology():
-    "Creates the specified WAN network topology."
+NUM_SUCURSALES = 2
 
-    net = Mininet(controller=None, link=TCLink, host=Host)
+def networkTopology(num_sucursales=NUM_SUCURSALES, net=None, start_cli=True):
+    # ... rest of your function ...
+    "Creates a WAN topology with up to 6 branches."
+    from mininet.net import Mininet
+    from mininet.node import Host
+    from mininet.cli import CLI
+    from mininet.link import TCLink
+    from mininet.log import info
 
-    info('*** Adding hosts and routers\n')
+    if net is None:
+        net = Mininet(controller=None, link=TCLink, host=Host)
+
+    info('*** Adding central router\n')
     r_central = net.addHost('r_central', ip=None)
-    r_suc1 = net.addHost('r_suc1', ip=None)
-    r_suc2 = net.addHost('r_suc2', ip=None)
-    h_suc1 = net.addHost('h_suc1', ip='10.0.1.254/24', defaultRoute='via 10.0.1.1')
-    h_suc2 = net.addHost('h_suc2', ip='10.0.2.254/24', defaultRoute='via 10.0.2.1')
 
-    info('*** Creating links\n')
-    net.addLink(r_central, r_suc1, intfName1='r_central-eth0', params1={'ip': '192.168.100.6/29'},
-                intfName2='r_suc1-eth0', params2={'ip': '192.168.100.1/29'})
-    net.addLink(r_central, r_suc2, intfName1='r_central-eth1', params1={'ip': '192.168.100.14/29'},
-                intfName2='r_suc2-eth0', params2={'ip': '192.168.100.9/29'})
-    net.addLink(r_suc1, h_suc1, intfName1='r_suc1-eth1', params1={'ip': '10.0.1.1/24'},
-                intfName2='h_suc1-eth0')
-    net.addLink(r_suc2, h_suc2, intfName1='r_suc2-eth1', params1={'ip': '10.0.2.1/24'},
-                intfName2='h_suc2-eth0')
+    routers = []
+    hosts = []
+
+    for i in range(1, num_sucursales + 1):
+        # Routers and hosts names
+        r_name = f"r_suc{i}"
+        h_name = f"h_suc{i}"
+
+        # WAN subnet base for this branch
+        wan_subnet_base = 8 * (i - 1)
+        wan_router_ip = f"192.168.100.{wan_subnet_base + 1}/29"
+        wan_central_ip = f"192.168.100.{wan_subnet_base + 6}/29"
+
+        # LAN subnet base for this branch
+        lan_router_ip = f"10.0.{i}.1/24"
+        host_ip = f"10.0.{i}.254/24"
+        host_gw = f"10.0.{i}.1"
+
+        # Add branch router and host
+        r_suc = net.addHost(r_name, ip=None)
+        h_suc = net.addHost(h_name, ip=host_ip, defaultRoute=f"via {host_gw}")
+        routers.append(r_suc)
+        hosts.append(h_suc)
+
+        # WAN link between central and branch router
+        net.addLink(r_central, r_suc,
+                    intfName1=f"r_central-eth{i-1}", params1={'ip': wan_central_ip},
+                    intfName2=f"{r_name}-eth0", params2={'ip': wan_router_ip})
+
+        # LAN link between branch router and host
+        net.addLink(r_suc, h_suc,
+                    intfName1=f"{r_name}-eth1", params1={'ip': lan_router_ip},
+                    intfName2=f"{h_name}-eth0")
 
     info('*** Starting network\n')
     net.build()
 
     info('*** Enabling IP forwarding on routers\n')
-    for router in [r_central, r_suc1, r_suc2]:
-        router.cmd('sysctl -w net.ipv4.ip_forward=1')
+    r_central.cmd('sysctl -w net.ipv4.ip_forward=1')
+    for r in routers:
+        r.cmd('sysctl -w net.ipv4.ip_forward=1')
 
     info('*** Configuring static routes\n')
-    r_central.cmd('ip route add 10.0.1.0/24 via 192.168.100.1 dev r_central-eth0')
-    r_central.cmd('ip route add 10.0.2.0/24 via 192.168.100.9 dev r_central-eth1')
-    r_suc1.cmd('ip route add default via 192.168.100.6 dev r_suc1-eth0')
-    r_suc2.cmd('ip route add default via 192.168.100.14 dev r_suc2-eth0')
+    for i, r_suc in enumerate(routers, 1):
+        wan_subnet_base = 8 * (i - 1)
+        wan_router_ip = f"192.168.100.{wan_subnet_base + 1}"
+        wan_central_ip = f"192.168.100.{wan_subnet_base + 6}"
+        lan_subnet = f"10.0.{i}.0/24"
+
+        # Central router: route to each branch LAN via branch WAN IP
+        r_central.cmd(f"ip route add {lan_subnet} via {wan_router_ip} dev r_central-eth{i-1}")
+        # Branch router: default route via central WAN IP
+        r_suc.cmd(f"ip route add default via {wan_central_ip} dev {r_suc.name}-eth0")
 
     info('*** Network is ready. Starting CLI...\n')
-    CLI(net)
-    net.stop()
-
-if __name__ == '__main__':
-    setLogLevel('info')
-    networkTopology()
+    if start_cli:
+        CLI(net)
+        net.stop()
+    return net
